4fe2090227aa541f75b3e5aed9200532a3ef57f4
Hanna <theannalios@gmail.com>
2023-03-08 17:43:52 +0300

    Exception handler is added

---
+++ src/main/java/com/solvd/movie/web/controller/AdviceController.java
@@ -0,0 +1,46 @@
+package com.solvd.movie.web.controller;
+
+import com.solvd.movie.domain.exception.ResourceNotFoundException;
+import com.solvd.movie.web.dto.ExceptionDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@Slf4j
+@RestControllerAdvice
+public class AdviceController {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ExceptionDto handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ExceptionDto.builder()
+                .message(ex.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public List<ExceptionDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        return ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> ExceptionDto.builder()
+                        .field(error.getObjectName() + "." + error.getField())
+                        .message(error.getDefaultMessage())
+                        .build())
+                .toList();
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ExceptionDto handleOtherExceptions(Exception ex) {
+        log.error(ex.getMessage(), ex);
+        return ExceptionDto.builder()
+                .message("Please, try later!")
+                .build();
+    }
+
+}
