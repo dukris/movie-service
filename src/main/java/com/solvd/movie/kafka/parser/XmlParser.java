8209cfe7f278c37c8d6a572b70cb65326da1ecc2
Hanna <theannalios@gmail.com>
2023-03-14 15:19:50 +0300

    XmlParser for XML data is implemented, resolved #9

---
+++ src/main/java/com/solvd/movie/kafka/parser/XmlParser.java
@@ -0,0 +1,23 @@
+package com.solvd.movie.kafka.parser;
+
+import com.jcabi.xml.XML;
+import com.jcabi.xml.XMLDocument;
+import lombok.SneakyThrows;
+
+import java.util.Objects;
+
+public class XmlParser {
+
+    @SneakyThrows
+    public static String getValue(String node) {
+        XML xml = new XMLDocument(Objects.requireNonNull(
+                XmlParser.class
+                        .getClassLoader()
+                        .getResource("producer.xml")));
+        return xml.nodes("//" + node)
+                .get(0)
+                .xpath("text()")
+                .get(0);
+    }
+
+}
