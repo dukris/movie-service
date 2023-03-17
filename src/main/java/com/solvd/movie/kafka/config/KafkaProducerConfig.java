9573b646878465cd28a8b4149663cdc276a36fad
Hanna <theannalios@gmail.com>
2023-03-17 18:10:30 +0300

    Code is updated

--- src/main/java/com/solvd/movie/kafka/config/KafkaProducerConfig.java
+++ src/main/java/com/solvd/movie/kafka/config/KafkaProducerConfig.java
@@ -1,12 +1,14 @@
 package com.solvd.movie.kafka.config;
 
 import com.solvd.movie.kafka.parser.XmlParser;
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
 import reactor.kafka.sender.KafkaSender;
 import reactor.kafka.sender.SenderOptions;
 
@@ -26,6 +28,13 @@ public class KafkaProducerConfig {
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, XmlParser.getValue("keySerializer"));
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, XmlParser.getValue("valueSerializer"));
         return SenderOptions.create(props);
+    }
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        return new KafkaAdmin(configs);
     }
 
     @Bean
