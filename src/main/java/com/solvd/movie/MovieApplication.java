9492b02b59789beb9309c27dd15e7f3132f29256
Hanna <theannalios@gmail.com>
2023-03-21 10:25:39 +0300

    Code is updated

--- src/main/java/com/solvd/movie/MovieApplication.java
+++ src/main/java/com/solvd/movie/MovieApplication.java
@@ -2,10 +2,8 @@ package com.solvd.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@ConfigurationPropertiesScan
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MovieApplication {
