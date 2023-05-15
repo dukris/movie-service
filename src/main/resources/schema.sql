83c82e6e2ea3f80ce91d626e5b23b17499d47c9a
hizmailovich <theannalios@gmail.com>
2023-05-15 18:42:57 +0300

    Schema is updated

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,13 @@
 CREATE TABLE IF NOT EXISTS movies
 (
-    id          bigserial    NOT NULL,
-    name        varchar(50)  NOT NULL,
-    year        integer      NOT NULL,
-    country     varchar(50)  NOT NULL,
-    genre       varchar(50)  NOT NULL,
-    language    varchar(50)  NOT NULL,
-    quality     integer      NOT NULL,
-    description varchar(150) NOT NULL,
+    id           bigserial    NOT NULL,
+    name         varchar(50)  NOT NULL,
+    year         integer      NOT NULL,
+    country      varchar(50)  NOT NULL,
+    genre        varchar(50)  NOT NULL,
+    language     varchar(50)  NOT NULL,
+    quality_from integer      NOT NULL,
+    quality_to   integer      NOT NULL,
+    description  varchar(150) NOT NULL,
     PRIMARY KEY (id)
 );
