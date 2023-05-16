c9aa32e9e496c60059fbc15d5ed8376d1a022934
hizmailovich <theannalios@gmail.com>
2023-05-16 12:05:13 +0300

    Tests are updated

--- src/test/java/com/solvd/movie/service/impl/MovieFactory.java
+++ src/test/java/com/solvd/movie/service/impl/MovieFactory.java
@@ -2,23 +2,36 @@ package com.solvd.movie.service.impl;
 
 import com.solvd.movie.model.EsMovie;
 import com.solvd.movie.model.Movie;
+import com.solvd.movie.model.Quality;
 
 public final class MovieFactory {
 
-    public static EsMovie getEsMovie(){
+    public static EsMovie getEsMovie() {
         EsMovie movie = new EsMovie();
         movie.setId(1L);
-        movie.setName("Name");
-        movie.setYear(2023);
+        movie.setName("Film");
+        movie.setYear(2019);
+        movie.setLanguage("language");
+        movie.setCountry("USA");
+        movie.setGenre("genre");
+        Quality quality = new Quality();
+        quality.setFrom(360);
+        quality.setTo(1024);
+        movie.setQuality(quality);
         return movie;
     }
 
-    public static Movie getMovie(){
+    public static Movie getMovie() {
         Movie movie = new Movie();
         movie.setId(1L);
-        movie.setName("Name");
+        movie.setName("Film");
+        movie.setYear(2019);
+        movie.setLanguage("language");
+        movie.setCountry("USA");
+        movie.setGenre("genre");
+        movie.setQualityFrom(360);
+        movie.setQualityTo(1024);
         movie.setDescription("Description");
-        movie.setYear(2023);
         return movie;
     }
 
