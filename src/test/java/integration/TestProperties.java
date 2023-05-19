eed1ca62278ed739ab81fb0af2c6ce5f436fa94e
hizmailovich <theannalios@gmail.com>
2023-05-19 17:16:11 +0300

    Test property is updated

--- src/test/java/integration/TestProperties.java
+++ src/test/java/integration/TestProperties.java
@@ -7,9 +7,6 @@ public class TestProperties {
     public void set(final DynamicPropertyRegistry registry){
         registry.add("spring.elasticsearch.rest.uris", () -> "localhost");
         registry.add("kafka.bootstrap-servers", () -> "localhost");
-//        registry.add("spring.r2dbc.url", () -> "localhost");
-//        registry.add("spring.r2dbc.username", () -> "test");
-//        registry.add("spring.r2dbc.password", () -> "test");
     }
 
 }
