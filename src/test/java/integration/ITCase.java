e4a44e3534230f9ba18d361dcdf2d70a37536cff
hizmailovich <theannalios@gmail.com>
2023-05-22 18:13:17 +0300

    Junit dependency is removed

--- src/test/java/integration/ITCase.java
+++ src/test/java/integration/ITCase.java
@@ -6,14 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-@Testcontainers
 public class ITCase {
 
-    @Container
     private static final ElasticsearchContainer container = new EsContainer().init();
 
     @DynamicPropertySource
