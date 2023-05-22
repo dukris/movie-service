ae59ea1a4d28c09c2566e47affe7723bf6eedfbe
hizmailovich <theannalios@gmail.com>
2023-05-22 16:56:50 +0300

    Profile is removed

--- src/test/java/integration/TestConfig.java
+++ src/test/java/integration/TestConfig.java
@@ -2,12 +2,12 @@ package integration;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.repository.config.EnableReactiveElasticsearchRepositories;
 
 
-@Configuration
+@TestConfiguration
 @EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 @ComponentScan(basePackages = "com.solvd.movie.persistence")
 @EnableReactiveElasticsearchRepositories(basePackages = "com.solvd.movie.persistence")
